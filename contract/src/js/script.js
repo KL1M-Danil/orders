430491a4973859b0c5e8afa25fdeddc019a5e77c
Danil <danil@MacBook-Pro-Danil.local>
2018-12-20 14:59:41 +0200

    adds all icons and promo img

--- contract/src/js/script.js
+++ contract/src/js/script.js
@@ -10,3 +10,50 @@ function onIconClick(event) {
   list.classList.toggle("nav-list-close");
   nav.classList.toggle("nav-close");
 }
+
+//E-mail Ajax Send
+$("form.класс-формы").submit(function() {
+  //Change
+  var th = $(this);
+  $.ajax({
+    type: "POST",
+    url: "/mail.php", //Change
+    data: th.serialize()
+  }).done(function() {
+    $(th)
+      .find(".success - класс уведомление об отправке")
+      .addClass("active")
+      .css("display", "flex")
+      .hide()
+      .fadeIn();
+    setTimeout(function() {
+      $(th)
+        .find(".success")
+        .removeClass("active")
+        .fadeOut();
+      th.trigger("reset");
+    }, 1000);
+  });
+  return false;
+});
+
+
+//Плавность якорей
+
+$(document).ready(function(){
+ 
+//ID блока с ссылками #prime_nav
+$("#nav").on("click","a", function (event) {
+//отменяем стандартную обработку нажатия по ссылке
+event.preventDefault();
+
+//забираем идентификатор бока с атрибута href
+var id  = $(this).attr('href'),
+
+//узнаем высоту от начала страницы до блока на который ссылается якорь
+top = $(id).offset().top;
+
+//анимируем переход на расстояние - top за 1500 мс
+$('body,html').animate({scrollTop: top}, 1200);
+});
+});
